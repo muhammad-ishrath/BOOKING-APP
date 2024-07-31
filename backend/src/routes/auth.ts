445623a9d6abcbe198d60c4a7795df543d98783a
muhammad-ishrath <muhammadishrath3@gmail.com>
2024-07-31 19:27:55 +0530

    Checked email and password  with  database

--- backend/src/routes/auth.ts
+++ backend/src/routes/auth.ts
@@ -1,7 +1,41 @@
-import express from 'express';
+import express, {Request, Response} from 'express';
+import { check, validationResult } from 'express-validator';
+import User from '../models/user';
+import bcrypt from 'bcryptjs';
 
 const router = express.Router();
 
 
 // user login api - api/users/login
-router.post("/login",[ check("email": "")])
+router.post("/login",[
+    check("email","Email is required").isEmail(), 
+    check("password", "Password with 6 or more characters Required").isLength({min:6})
+], async (req: Request, res:Response) => {
+
+    const errors = validationResult(req);
+    if(!errors.isEmpty()){
+        return res.status(400).json({message:errors.array()});
+    }
+
+    const {email, password} = req.body;
+
+    try {
+        const user = await User.findOne({email});
+
+        if (!user) {
+            return res.status(400).json({message:"Invalid Credentials"});
+        }
+
+        const isMatch = await bcrypt.compare(password, user.password);
+        if (!isMatch) {
+            return res.status(400).json({message:"Invalid Credentials"});
+        }
+
+    } catch (error) {
+        console.log(error);
+        res.status(500).json({message: "something went wrong"});
+    }
+
+    
+
+});
